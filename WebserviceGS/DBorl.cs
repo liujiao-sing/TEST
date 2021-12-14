fdf7da9b25ba7f3e3c28ab5bebe2f38215482463
LIUJIAO <64250686+liujiao-sing@users.noreply.github.com>
2021-12-14 16:13:26 +0800

    Add files via upload

---
+++ WebserviceGS/DBorl.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OracleClient;
+using System.Linq;
+using System.Web;
+
+namespace WebserviceGS
+{
+    public class DBorl
+    {
+        public static string connectionString = "Data Source=(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.1.19)(PORT = 1521)))(CONNECT_DATA = (SERVICE_NAME = ORCL)));Persist Security Info=True;User Id = abtdev;Password=abt_dev";
+
+        #region 公用方法
+
+        public static int GetMaxID(string FieldName, string TableName)
+        {
+            string strsql = "select max(" + FieldName + ")+1 from " + TableName;
+            object obj = GetSingle(strsql);
+            if (obj == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return int.Parse(obj.ToString());
+            }
+        }
+        public static bool Exists(string strSql)
+        {
+            object obj = GetSingle(strSql);
+            int cmdresult;
+            if ((Object.Equals(obj, null)) || (Object.Equals(obj, System.DBNull.Value)))
+            {
+                cmdresult = 0;
+            }
+            else
+            {
+                cmdresult = int.Parse(obj.ToString());
+            }
+            if (cmdresult == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool Exists(string strSql, params OracleParameter[] cmdParms)
+        {
+            object obj = GetSingle(strSql, cmdParms);
+            int cmdresult;
+            if ((Object.Equals(obj, null)) || (Object.Equals(obj, System.DBNull.Value)))
+            {
+                cmdresult = 0;
+            }
+            else
+            {
+                cmdresult = int.Parse(obj.ToString());
+            }
+            if (cmdresult == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 执行一条计算查询结果语句，返回查询结果（object）。
+        /// </summary>
+        /// <param name="SQLString">计算查询结果语句</param>
+        /// <returns>查询结果（object）</returns>
+        public static object GetSingle(string SQLString)
+        {
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                using (OracleCommand cmd = new OracleCommand(SQLString, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        object obj = cmd.ExecuteScalar();
+                        if ((Object.Equals(obj, null)) || (Object.Equals(obj, System.DBNull.Value)))
+                        {
+                            return null;
+                        }
+                        else
+                        {
+                            return obj;
+                        }
+                    }
+                    catch (System.Data.OracleClient.OracleException e)
+                    {
+                        connection.Close();
+                        throw new Exception(e.Message);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 执行一条计算查询结果语句，返回查询结果（object）。
+        /// </summary>
+        /// <param name="SQLString">计算查询结果语句</param>
+        /// <returns>查询结果（object）</returns>
+        public static object GetSingle(string SQLString, params OracleParameter[] cmdParms)
+        {
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                using (OracleCommand cmd = new OracleCommand())
+                {
+                    try
+                    {
+                        PrepareCommand(cmd, connection, null, SQLString, cmdParms);
+                        object obj = cmd.ExecuteScalar();
+                        cmd.Parameters.Clear();
+                        if ((Object.Equals(obj, null)) || (Object.Equals(obj, System.DBNull.Value)))
+                        {
+                            return null;
+                        }
+                        else
+                        {
+                            return obj;
+                        }
+                    }
+                    catch (System.Data.OracleClient.OracleException e)
+                    {
+                        throw new Exception(e.Message);
+                    }
+                }
+            }
+        }
+
+        private static void PrepareCommand(OracleCommand cmd, OracleConnection conn, OracleTransaction trans, string cmdText, OracleParameter[] cmdParms)
+        {
+            if (conn.State != ConnectionState.Open)
+                conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = cmdText;
+            if (trans != null)
+                cmd.Transaction = trans;
+            cmd.CommandType = CommandType.Text;//cmdType;
+            if (cmdParms != null)
+            {
+                foreach (OracleParameter parm in cmdParms)
+                    cmd.Parameters.Add(parm);
+            }
+        }
+
+        public static int ExecuteSql(string SQLString)
+        {
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                using (OracleCommand cmd = new OracleCommand(SQLString, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        int rows = cmd.ExecuteNonQuery();
+                        return rows;
+                    }
+                    catch (System.Data.OracleClient.OracleException E)
+                    {
+                        connection.Close();
+                        throw new Exception(E.Message);
+                    }
+                }
+            }
+        }
+
+        public static int ExecuteSql(string SQLString,string clob)
+        {
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                using (OracleCommand cmd = new OracleCommand(SQLString, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        OracleParameter op = new OracleParameter("clob", OracleType.Clob);
+                        op.Value = clob;
+                        cmd.Parameters.Add(op);
+                        int rows = cmd.ExecuteNonQuery();                      
+                        return rows;
+                    }
+                    catch (System.Data.OracleClient.OracleException E)
+                    {
+                        connection.Close();
+                        throw new Exception(E.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WebserviceGS/WebService1.asmx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebserviceGS
+{
+    /// <summary>
+    /// WebService1 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string ERP_TO_MES_DataTransport(string Factory, string task_name, string descript, string create_time, string creator, string operatype,
+                                               string source_table, string target_table, string data, string data_inserted, string data_deleted)
+        {
+            string res = "";
+            try
+            {
+                //List<SqlParameter> list = new List<SqlParameter>();
+                //list.Add(new SqlParameter("@task_name", task_name));
+                //list.Add(new SqlParameter("@descript", descript));
+                //list.Add(new SqlParameter("@create_time", create_time));
+                //list.Add(new SqlParameter("@creator", creator));
+                //list.Add(new SqlParameter("@operatype", operatype));
+                //list.Add(new SqlParameter("@source_table", source_table));
+                //list.Add(new SqlParameter("@target_table", target_table));
+                //list.Add(new SqlParameter("@data", data));
+                //list.Add(new SqlParameter("@data_inserted", data_inserted));
+                //list.Add(new SqlParameter("@data_deleted", data_deleted));
+
+
+                //     IDataParameter[] parametert = new IDataParameter[]
+                //{
+                //      new OracleParameter("task_name", OracleType.NVarChar,500),
+                //      new OracleParameter("descript",OracleType.NVarChar,500),
+                //      new OracleParameter("create_time",OracleType.NVarChar,500),
+                //      new OracleParameter("creator",OracleType.NVarChar,500),
+                //      new OracleParameter("operatype",OracleType.NVarChar,500),
+                //      new OracleParameter("source_table",OracleType.NVarChar,500),
+                //      new OracleParameter("target_table",OracleType.NVarChar,500),
+                //      new OracleParameter("data",OracleType.DateTime),
+                //      new OracleParameter("data_inserted",OracleType.Blob),
+                //      new OracleParameter("data_deleted",OracleType.NVarChar,500)
+
+                //};
+                //     parametert[0].Value = task_name;
+                //     parametert[1].Value = descript;
+                //     parametert[2].Value = create_time;
+                //     parametert[3].Value = creator;
+                //     parametert[4].Value = operatype;
+                //     parametert[5].Value = source_table;
+                //     parametert[6].Value = target_table;
+                //     parametert[7].Value = data;
+                //     parametert[8].Value = data_inserted;
+                //     parametert[9].Value = data_deleted;
+                //     Dictionary<string, string> keyValuest = DbHelperOra.RunProcedure("Prc_Erp_MES_send", parametert);
+
+                int li_count = DBorl.ExecuteSql(" INSERT INTO SYSDEC(task_id,task_name,descript,create_time,creator,operatype,source_table,target_table,data,data_inserted,data_deleted,states) " +
+                           " VALUES(seq_down.nextval, '" + task_name + "', '" + descript + "', '" + create_time + "', '" + creator + "', '" + operatype + "', '" + source_table + "', '" + target_table + "', '" + data + "', '" + data_inserted + "', '" + data_deleted + "','NEW')");
+                if (li_count <= 0)
+                {
+                    res = "ERROR:" + "数值错误";
+                }
+            }
+            catch (Exception ex)
+            {
+                res = "ERROR:" + ex.Message;
+            }
+            return res;
+        }
+    }
+}
